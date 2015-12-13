7e1b4be7315408364c369588c7c666e7eacfda5b
unknown <e.w.nickerson@gmail.com>
2015-12-13 15:04:21 -0800

    first commit

---
+++ JsonHelper.cs
@@ -0,0 +1,59 @@
+﻿using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+public class JsonHelper
+{
+
+    public static string GetJsonObject(string jsonString, string handle)
+    {
+        string pattern = "\"" + handle + "\"\\s*:\\s*\\{";
+
+        Regex regx = new Regex(pattern);
+
+        Match match = regx.Match(jsonString);
+        
+        if (match.Success)
+        {
+            int bracketCount = 1;
+            int i;
+            int startOfObj = match.Index + match.Length;
+            for (i = startOfObj; bracketCount > 0; i++)
+            {
+                if (jsonString[i] == '{') bracketCount++;
+                else if (jsonString[i] == '}') bracketCount--;
+            }
+            return "{" + jsonString.Substring(startOfObj, i - startOfObj);
+        }
+
+        //no match, return null
+        return null;
+    }
+
+    public static string[] GetJsonObjects(string jsonString, string handle)
+    {
+        string pattern = "\"" + handle + "\"\\s*:\\s*\\{";
+
+        Regex regx = new Regex(pattern);
+
+        //check if there's a match at all, return null if not
+        if (!regx.IsMatch(jsonString)) return null;
+
+        List<string> jsonObjList = new List<string>();
+
+        //find each regex match
+        foreach (Match match in regx.Matches(jsonString))
+        {
+            int bracketCount = 1;
+            int i;
+            int startOfObj = match.Index + match.Length;
+            for (i = startOfObj; bracketCount > 0; i++)
+            {
+                if (jsonString[i] == '{') bracketCount++;
+                else if (jsonString[i] == '}') bracketCount--;
+            }
+            jsonObjList.Add("{" + jsonString.Substring(startOfObj, i - startOfObj));
+        }
+
+        return jsonObjList.ToArray();
+    }
+}
